5738422a9e00098db27c853c2513ef508a9e4639
mli <maximilian.lincks@dscsag.com>
2022-07-13 08:58:14 +0200

    added Expense Items

---
+++ src/components/ExpenseItem.tsx
@@ -0,0 +1,18 @@
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+
+type expense = {id: string, title : string, amount : number, date : Date}
+
+function ExpeseItem(props : {expense: expense}) {
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.expense.date}/>
+      <div className="expense-item__description">
+        <h2>{props.expense.title}</h2>
+        <div className="expense-item__price">{props.expense.amount}€</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpeseItem;
