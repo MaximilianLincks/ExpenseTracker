011d181354d7e2e114948b3021971360aca1fcf9
mli <maximilian.lincks@dscsag.com>
2022-07-13 08:56:35 +0200

    added Expense Items

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import './App.css';
+import ExpeseItem from "./components/ExpenseItem";
+import expenses from "./data/expenses";
 
 function App() {
   return (
     <>
       <h2>Let's get started!</h2>
+      {expenses.map(expense => <ExpeseItem expense={expense} />)}
     </>
   );
 }
