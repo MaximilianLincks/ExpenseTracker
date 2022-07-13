72fee0f95311ed60e18331a6e3af5cd1ac684f9d
mli <maximilian.lincks@dscsag.com>
2022-07-13 11:43:58 +0200

    added form to create new Expenses

---
+++ src/components/NewExpense/NewExpense.tsx
@@ -0,0 +1,12 @@
+import "./styles/NewExpense.css";
+import ExpenseForm from "./ExpenseFrom";
+
+const NewExpense = () => {
+  return (
+    <div className="new-expense">
+      <ExpenseForm />
+    </div>
+  );
+};
+
+export default NewExpense;
